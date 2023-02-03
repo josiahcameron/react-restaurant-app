ee38f3b48771feb761732e3a0de7f4b9968def34
josiahcameron <josiahcameron2015@gmail.com>
2023-02-03 18:59:00 -0500

    Added Button for Testing

--- src/App.js
+++ src/App.js
@@ -1,36 +1,62 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Nav from "react-bootstrap/Nav";
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image'
 
-const menuItem = {
-  name: "name",
-  description: "description",
-  price: "$12.00",
-  img: "url",
-};
+
+
+function Menu(){
+const menuItems = 
+{ 
+  name: "spaghetti", 
+  description: "Pciriole spirali tortellini conchiglioni lumache.", 
+  price: "$11.50", 
+  img: "#"};
+
+return(
+  <>
+  <div className="mb-2">
+    <Button variant="primary" size="sm">
+      Large button
+      <Image
+        src=
+        "https://www.publicdomainpictures.net/pictures/20000/nahled/spaghetti-bolognese-11291665127QGd.jpg"
+      className="w-25 p-3" />
+    </Button>
+  </div>
+</>
+)
+}
+
+
+
 
 function App() {
   return (
-    <Nav className="Menu"
-      activeKey="/home"
-      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-    >
-      <Nav.Item>
-        <Nav.Link href="/home">All Items</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-1">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-2">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="disabled" disabled>
-          Disabled
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
+      <div>
+          <Menu />
+      </div>
+//     // <Nav className="Menu"
+//     //   activeKey="/home"
+//     //   onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+//     // >
+//     //   <Nav.Item>
+//     //     <Nav.Link href="/home">All Items</Nav.Link>
+//     //   </Nav.Item>
+//     //   <Nav.Item>
+//     //     <Nav.Link eventKey="link-1">Entrees</Nav.Link>
+//     //   </Nav.Item>
+//     //   <Nav.Item>
+//     //     <Nav.Link eventKey="link-2">Desserts</Nav.Link>
+//     //   </Nav.Item>
+//     //   <Nav.Item>
+//     //   <Nav.Link eventKey="link-3">View Order</Nav.Link>
+//     //   </Nav.Item>
+//     // </Nav>
   );
 }
+
+
+
 
 export default App;
