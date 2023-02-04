f885103ac1b8c74ec951b7991ff98ce4ef9f73c3
josiahcameron <josiahcameron2015@gmail.com>
2023-02-04 12:11:35 -0500

    Added HTML Elements to Menu / Added Base Bootstrap for View Order Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import Nav from "react-bootstrap/Nav";
 import Button from 'react-bootstrap/Button';
-import Image from 'react-bootstrap/Image'
-
+import Card from 'react-bootstrap/Card';
+import Collapse from 'react-bootstrap/Collapse';
 
 
 function Menu(){
@@ -16,43 +16,76 @@ const menuItems =
 return(
   <>
   <div className="mb-2">
-    <Button variant="primary" size="sm">
-      Large button
-      <Image
-        src=
-        "https://www.publicdomainpictures.net/pictures/20000/nahled/spaghetti-bolognese-11291665127QGd.jpg"
-      className="w-25 p-3" />
+  <Button variant="outline-secondary" className="menu-item" size="md">
+    <div className="card-header">
+      <span className="menu-item-name">Spaghetti</span>
+    </div>
+    <div>
+    <span className="menu-item-description">Pciriole spirali tortellini conchiglioni lumache.</span>
+    </div>
+    <div className="price">
+    <span className="item-price">$12.00</span>
+    </div>
+    <img src="/src/Assets/images/spaghetti.png" />
     </Button>
   </div>
 </>
 )
 }
 
+function ViewOrder() {
+  const [open, setOpen] = useState();
+
+  return (
+    <>
+      <Button
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+      >
+        click
+      </Button>
+      <div style={{ minHeight: '150px' }}>
+        <Collapse in={open} dimension="width">
+          <div id="example-collapse-text">
+            <Card body style={{ width: '400px' }}>
+              Anim pariatur cliche reprehenderit, enim eiusmod high life
+              accusamus terry richardson ad squid. Nihil anim keffiyeh
+              helvetica, craft beer labore wes anderson cred nesciunt sapiente
+              ea proident.
+            </Card>
+          </div>
+        </Collapse>
+      </div>
+    </>
+  );
+}
 
 
 
 function App() {
   return (
       <div>
-          <Menu />
-      </div>
-//     // <Nav className="Menu"
-//     //   activeKey="/home"
-//     //   onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-//     // >
-//     //   <Nav.Item>
-//     //     <Nav.Link href="/home">All Items</Nav.Link>
-//     //   </Nav.Item>
-//     //   <Nav.Item>
-//     //     <Nav.Link eventKey="link-1">Entrees</Nav.Link>
-//     //   </Nav.Item>
-//     //   <Nav.Item>
-//     //     <Nav.Link eventKey="link-2">Desserts</Nav.Link>
-//     //   </Nav.Item>
-//     //   <Nav.Item>
-//     //   <Nav.Link eventKey="link-3">View Order</Nav.Link>
-//     //   </Nav.Item>
-//     // </Nav>
+    <Nav className="Menu"
+      activeKey="/home"
+      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    >
+      <Nav.Item>
+        <Nav.Link href="/home">All Items</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Entrees</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Desserts</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+      <Nav.Link eventKey="link-3">View Order</Nav.Link>
+      </Nav.Item>
+    </Nav>
+    <Menu />
+    <ViewOrder />
+    </div>
   );
 }
 
