dad6b308fa080627036087dbe25879cf6d5b8ad4
josiahcameron <josiahcameron2015@gmail.com>
2023-02-04 15:18:59 -0500

    Added NavBar / Added Order Form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import "./App.css";
+import { useState } from "react";
 import Nav from "react-bootstrap/Nav";
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Collapse from 'react-bootstrap/Collapse';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import "./styles/navbar.css";
+
+
+
+
+
 
 
 function Menu(){
@@ -16,7 +28,7 @@ const menuItems =
 return(
   <>
   <div className="mb-2">
-  <Button variant="outline-secondary" className="menu-item" size="md">
+  <Button variant="outline-secondary" className="menu-item" size="md" >
     <div className="card-header">
       <span className="menu-item-name">Spaghetti</span>
     </div>
@@ -33,6 +45,54 @@ return(
 )
 }
 
+function OrderForm() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
 function ViewOrder() {
   const [open, setOpen] = useState();
 
@@ -42,11 +102,12 @@ function ViewOrder() {
         onClick={() => setOpen(!open)}
         aria-controls="example-collapse-text"
         aria-expanded={open}
+        
       >
         click
       </Button>
       <div style={{ minHeight: '150px' }}>
-        <Collapse in={open} dimension="width">
+        <Collapse in={open} dimension="width" >
           <div id="example-collapse-text">
             <Card body style={{ width: '400px' }}>
               Anim pariatur cliche reprehenderit, enim eiusmod high life
@@ -61,30 +122,33 @@ function ViewOrder() {
   );
 }
 
+function ChooseMenu() {
+  return (
+    <Navbar bg="light" expand="lg" fixed="top" >
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#">All Items</Nav.Link>
+            <Nav.Link href="#">Entrees</Nav.Link>
+            <Nav.Link href="#">Desserts</Nav.Link>
+            <Nav.Link href="#">Sides</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
 
 
 function App() {
   return (
-      <div>
-    <Nav className="Menu"
-      activeKey="/home"
-      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-    >
-      <Nav.Item>
-        <Nav.Link href="/home">All Items</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-1">Entrees</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-2">Desserts</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-      <Nav.Link eventKey="link-3">View Order</Nav.Link>
-      </Nav.Item>
-    </Nav>
-    <Menu />
-    <ViewOrder />
+    
+    <div>
+    <ChooseMenu />
+    <Menu />;
+    <OrderForm />;
+    <ViewOrder />;
     </div>
   );
 }
