9cb29404d2c7fefd4f34d07a4c75423abde2e5b9
josiahcameron <josiahcameron2015@gmail.com>
2023-02-05 00:36:12 -0500

    Updated Modal Information

--- src/App.js
+++ src/App.js
@@ -30,20 +30,21 @@ function OrderForm() {
   return (
     <div>
       <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
+        Place Your Order Here
       </Button>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
+        <img className="selected-item-img" src="https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&w=800"/>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
+              <Form.Label>Enter Name</Form.Label>
               <Form.Control
-                type="email"
-                placeholder="name@example.com"
+                type="name"
+                placeholder="Your Name Here"
                 autoFocus
               />
             </Form.Group>
@@ -51,8 +52,8 @@ function OrderForm() {
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
             >
-              <Form.Label>Example textarea</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Label>Special Instructions</Form.Label>
+              <Form.Control as="textarea" rows={3} placeholder="Put Special Requests Here (Extra Utensils, Parmesan, Etc.)"/>
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -61,7 +62,7 @@ function OrderForm() {
             Close
           </Button>
           <Button variant="primary" onClick={handleClose}>
-            Save Changes
+            Add To Order
           </Button>
         </Modal.Footer>
       </Modal>
